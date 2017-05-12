46faef5b83453e5d6373293b68134e0fa7731dab
sysleg <zlou.sysleg@gmail.com>
2017-05-12 20:10:27 +0300

    Start project locator, реализован запрос разрешений для андроида 6

---
+++ app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -0,0 +1,39 @@
+package com.bignerdranch.android.locatr;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class LocatrActivity extends SingleFragmentActivity{
+    private static final int REQUEST_ERROR = 0;
+
+
+    @Override
+    protected Fragment createFragment() {
+        return LocatrFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GoogleApiAvailability availability = GoogleApiAvailability.getInstance();
+        int errorCode = availability.isGooglePlayServicesAvailable (this);
+        if(errorCode != ConnectionResult.SUCCESS){
+            Dialog errorDialog = availability.getErrorDialog(this, errorCode, REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            finish();
+                        }
+                    });
+            errorDialog.show();
+        }
+    }
+}
